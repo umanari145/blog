c0787e624b95bf398aa3dc1d50fcc2f215dd974e
umanari145 <umanari145@gmail.com>
2024-11-10 00:06:45 +0900

    blogデータの出力

--- mongo/load_contents.js
+++ mongo/load_contents.js
@@ -1,16 +1,21 @@
 import { promises as fs } from "fs";
 import * as yaml from "js-yaml";
-import { MongoClient } from "mongodb";
+//import { MongoClient } from "mongodb";
 import { readdir } from 'fs/promises';
-import { join, basename, dirname } from 'path';
+import { join, basename, dirname} from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dir } from "console";
+import { exit } from "process";
 
 // MongoDB の接続文字列とデータベース名を定数として定義
-const MONGO_URI = "mongodb://blog_user:blog_pass@mongo:27017";
-const DATABASE_NAME = "blog";
-const rootDirectory = './output/';  // 適切にパスを設定してください
+//const MONGO_URI = "mongodb://blog_user:blog_pass@mongo:27017";
+//const DATABASE_NAME = "blog";
+//const rootDirectory = './output/';  // 適切にパスを設定してください
 // テスト
-//const rootDirectory = './output_test/';  // 適切にパスを設定してください
-
+const rootDirectory = './output/';  // 適切にパスを設定してください
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 class LoadPosts {
 
     paths;
@@ -21,17 +26,17 @@ class LoadPosts {
 
     loadContents = async(rootDirectory) => {
         await this.findLatestIndexMd(rootDirectory);
-        let jsons = [];
+        let items = [];
         for (let i = 0; i < this.paths.length; i++) {
             const path = this.paths[i];
-            const json = await this.convertMarkDownToJson(path);
-            jsons.push(json);
-            if (jsons.length === 100) {
-                this.loadData(jsons);
-                jsons = [];
+            const item = await this.convertMarkDownToJson(path);
+            items.push(item);
+            if (items.length === 100) {
+                this.loadData(items);
+                items = [];
             }
         }
-        this.loadData(jsons);
+        this.loadData(items);
     }
 
     convertMarkDownToJson = async(filePath) => {
@@ -43,18 +48,46 @@ class LoadPosts {
         return metadata;
     }
 
-    async loadData(jsons) {
-        const client = new MongoClient(MONGO_URI);
-        try {
-            await client.connect();
-            const db = client.db(DATABASE_NAME);
-            await db.collection("posts").insertMany(jsons);
-        } catch (err) {
-            console.error("エラーが発生しました:", err);
-        } finally {
-            await client.close();
-        }
+    loadData(items) {
+        items.forEach((item, index) => {
+            const [year, month] = item.date.split('-');
+            const dirPath = path.join(__dirname, "blog_json", year, month);
+            // ディレクトリが存在しない場合は作成
+
+            //fs.mkdir(dirPath, { recursive: true }, (err) => {
+            //    if (err) {
+            //        console.error('Error creating directories', err);
+            //        return;
+            //    }
+            //});
+
+            if (dirPath) {
+                const filePath = path.join(dirPath, `${item.date}-${index}.json`);
+                fs.writeFile(filePath, JSON.stringify(item, null, 2), (err) => {
+                    if (err) {
+                        console.error('Error writing JSON file', err);
+                    } else {
+                        console.log(`File successfully written to ${filePath}`);
+                    }
+                });
+            } else {
+                console.log(`error dirPath:${dirPath} File: ${item.title}`)
+            }
+        }) 
     }
+
+    //async loadData(jsons) {
+    //    const client = new MongoClient(MONGO_URI);
+    //    try {
+    //        await client.connect();
+    //        const db = client.db(DATABASE_NAME);
+    //        await db.collection("posts").insertMany(jsons);
+    //    } catch (err) {
+    //        console.error("エラーが発生しました:", err);
+    //    } finally {
+    //        await client.close();
+    //    }
+    //}
 
 
     async searchDir(currentDir, callback) {
