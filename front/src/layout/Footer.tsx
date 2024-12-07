d7b2d8411025cf20109a12d066396c5f6d80b9aa
umanari145 <umanari145@gmail.com>
2024-12-08 00:01:16 +0900

    移管した状態を記載

--- front/src/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <p>&copy; 2024 技術ブログ. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-};
